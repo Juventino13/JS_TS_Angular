da11ba980401a30b7a66758b03141de959f18d38
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-03-25 00:48:07 -0600

    udemy-23

--- Angular_Udemy/my-first-app/src/app/app.module.ts
+++ Angular_Udemy/my-first-app/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule}from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular_Udemy/my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,13 @@
+import {Component}from '@angular/core'
+
+
+@Component({
+    selector: 'app-warning-alert ',
+    template:` 
+    <p> This is a warning, you are in a danger! </p>
+    `
+
+})
+export class WarningAlertComponent{
+
+}
