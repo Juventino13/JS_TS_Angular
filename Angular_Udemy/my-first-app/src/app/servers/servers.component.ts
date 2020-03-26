97b13ccc39f520e8cea14796f7e8123a700f4c3e
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-03-26 14:25:55 -0600

    binding

--- Angular_Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Angular_Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -4,16 +4,28 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  //template: `
+   //<app-server></app-server>
+  //<app-server></app-server>`
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer= false;
+  serverCreationStatus='No server was created'
+  constructor() { 
+    setTimeout(()=> {
+      this.allowNewServer=true;
+    },2000);
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
-
+onCreateServer(){
+  this.serverCreationStatus='Server was created!';
+}
+onUpdateServerName(event: any){
+  this.serverName=(<HTMLInputElement>event.target).value;
+}
 }
