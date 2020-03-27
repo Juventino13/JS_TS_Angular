a9533d9cebf63da9c5077e0b655dde37457a7919
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-03-27 11:33:42 -0600

    ngIf

--- Angular_Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Angular_Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -24,7 +24,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 onCreateServer(){
-  this.serverCreationStatus='Server was created!';
+  this.serverCreationStatus='Server was created! '+ this.serverName;
 }
 onUpdateServerName(event: any){
   this.serverName=(<HTMLInputElement>event.target).value;
