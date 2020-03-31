62db97102b2c04032697b84b5e1fc509570a0868
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-03-31 02:25:50 -0600

    section 3

--- Angular_Udemy/projectMS/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular_Udemy/projectMS/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://tiay08.files.wordpress.com/2012/02/pizza-1.jpg?w=584'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://sites.google.com/site/resetastipicashondurenas2/_/rsrc/1457012653966/resetas-tipi/image004.png')
+  ];
 
   constructor() { }
 
