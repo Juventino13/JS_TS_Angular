1bb7bdd5ba879b1dbc8ead79d35dc358564dac99
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-04-23 18:36:04 -0500

    binding 101

--- Angular_Udemy/directives-start/src/app/app.module.ts
+++ Angular_Udemy/directives-start/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { BasicHighLightDirective } from './basic.highlight/basic-highlight.directive';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighLightDirective
+    BasicHighLightDirective,
+    BetterHighlightDirective
 
   ],
   imports: [
