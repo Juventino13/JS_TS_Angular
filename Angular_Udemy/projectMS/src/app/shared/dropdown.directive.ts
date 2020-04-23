5773ba1fef2945528d2114c85fe1de45c375814a
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-04-23 18:59:46 -0500

    dropdown open

--- Angular_Udemy/projectMS/src/app/shared/dropdown.directive.ts
+++ Angular_Udemy/projectMS/src/app/shared/dropdown.directive.ts
@@ -1,7 +1,12 @@
-import { Directive } from '@angular/core';
-@Directive(
-    {selector:'[appDropdown]'
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+    selector:'[appDropdown]'
 })
 export class DropdownDirective{
+@HostBinding('class.open') isOpen= false;
 
+    @HostListener('click') toggleOpen(){
+    this.isOpen=!this.isOpen;
+}
 }
