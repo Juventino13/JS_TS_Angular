079d46b78aae82069e5d2e399aadab4bc69b6d1c
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-04-22 20:29:49 -0500

    appbasic

--- Angular_Udemy/directives-start/src/app/app.module.ts
+++ Angular_Udemy/directives-start/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighLightDirective } from './basic.highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighLightDirective
+
   ],
   imports: [
     BrowserModule,
