36259e339e4f536ad74051b04015867ac3ba9366
Juventino Perez <juperezpe@ittepic.edu.mx>
2020-04-23 18:47:55 -0500

    adding unlessDirective

--- Angular_Udemy/directives-start/src/app/app.module.ts
+++ Angular_Udemy/directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighLightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
 
   ],
   imports: [
